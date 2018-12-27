a76b3840d43e94799f5535757249b4acecafe0b2
chenzxl <czxl276@163.com>
2018-12-27 17:37:41 +0800

    修改

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: chenzhinjian
+ * @Date: 2018-12-25 22:19:45
+ * @LastEditTime: 2018-12-27 11:47:14
+ * @Description: 
+ */
+
+import dome from './components/dome/index.vue';
+
+export {
+  dome
+}
